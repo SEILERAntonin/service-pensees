99ef233358d2c7c70b678568a9a9be38851ad384
SEILERAntonin <43207267+SEILERAntonin@users.noreply.github.com>
2019-02-27 18:12:19 -0500

    Fusion projet visuel avec projet inspiration pour #1

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,5 +1,6 @@
 import java.util.Iterator;
 import java.util.List;
+
 import accesseur.PenseeDAO;
 import modele.Pensee;
 import outils.Journal;
@@ -8,7 +9,23 @@ public class App {
 
 	public static void main(String[] parametres) {
 
-		VueInspiration.launch(VueInspiration.class, parametres);
+		//Journal.activer();
+		Journal.activerNiveau(0);
+		
+		PenseeDAO penseeDAO = new PenseeDAO();
+		List<Pensee> listePensees = penseeDAO.listerPensees();
+		
+		for(Iterator<Pensee> visiteur = listePensees.iterator(); visiteur.hasNext(); )
+		{
+			Pensee pensee = visiteur.next();		
+			Journal.ecrire(5, pensee.getMessage() + "(" + pensee.getAuteur() + ")");
+		}
+
+		Pensee pensee = new Pensee("Rossetti","Ce qui est plus triste qu une oeuvre inachevee, c est une oeuvre jamais commencee.");
+		penseeDAO.ajouterPensee(pensee);		
+		
+		VueInspirationVisuelle.launch(VueInspirationVisuelle.class, parametres);
+		
 		
 	}
 
