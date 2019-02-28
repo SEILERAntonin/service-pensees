5411010206e1aac911e8e708e80d862b66cb3e08
SEILERAntonin <43207267+SEILERAntonin@users.noreply.github.com>
2019-02-27 19:45:37 -0500

    Ajout du champs source dans le service php pour #4

---
+++ Service/src/pensee/index_aleatoire.php
@@ -0,0 +1,24 @@
+// Auteur : Antonin Seiler INFO 2
+// Date : 21/02/2019
+// Sujet : Maintenance
+
+<?php 
+	include "../../accesseur/PenseeDAO.php";
+	$penseeDAO = new PenseeDAO();
+	//print_r($penseeDAO);
+	$listePensees = $penseeDAO->listerPensees();
+	//print_r($listePensees);
+	$indexAleatoire = rand(0, count($listePensees) - 1);
+	$pensee = $listePensees[$indexAleatoire];
+	// TODO optimiser en prenant un vrai random dans la BDD
+?><?php 
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<pensee>
+	<auteur><?=($pensee->auteur)?></auteur>
+	<message><?=($pensee->message)?></message>
+	<annee><?=$pensee->annee?></annee>
+	<source><?=$pensee->source?></source>
+	<id><?=$pensee->idPensee?></id>
+</pensee>
